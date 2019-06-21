7f8347180ea0c9f90da71683840655cf125a5546
Someone who forgot to `git pair` <dev.it@kly.id>
2019-06-21 16:15:56 +0700

    add optional in parameters

---
+++ typescript/function-optionalParameters.ts
@@ -0,0 +1,11 @@
+(function() {
+  function greet(
+    name: string,
+    age: number,
+    optimalParameterPlace?: string
+  ): void {
+    console.log(
+      `hello ! ${name}, your are is ${number}. welcome to ${optimalParameterPlace}`
+    );
+  }
+})();
