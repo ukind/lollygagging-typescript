0c996e71f34708424188a78ff4937303b6af51cb
Someone who forgot to `git pair` <dev.it@kly.id>
2019-06-21 15:33:03 +0700

    adding object keys for interface from external

--- js/interface-extends.js
+++ js/interface-extends.js
@@ -1,7 +1,19 @@
-const regularBert = {
-    name: "bert"
-};
-const singer = {
-    name: "Cael",
-    superpower: "sing"
-};
+(function () {
+    const regularBert = {
+        name: "bert"
+    };
+    const singer = {
+        name: "Cael",
+        superpower: "sing"
+    };
+})();
+// interface extra: add additional objecy key and value
+(function () {
+    let adminUser = {
+        name: "yogi",
+        age: 22,
+        lagu: "tikus makan sabun",
+        anotherKey: "example of another key"
+    };
+    console.log(adminUser.lagu);
+})();
--- typescript/interface-extends.ts
+++ typescript/interface-extends.ts
@@ -1,16 +1,38 @@
-interface IPerson {
-  name: string;
-}
+(function() {
+  interface IPerson {
+    name: string;
+  }
 
-interface IHero extends IPerson {
-  superpower: string;
-}
+  interface IHero extends IPerson {
+    superpower: string;
+  }
 
-const regularBert: IPerson = {
-  name: "bert"
-};
+  const regularBert: IPerson = {
+    name: "bert"
+  };
 
-const singer: IHero = {
-  name: "Cael",
-  superpower: "sing"
-};
+  const singer: IHero = {
+    name: "Cael",
+    superpower: "sing"
+  };
+})();
+
+// interface extra: add additional objecy key and value
+(function() {
+  interface IUser {
+    name: string;
+    readonly age: number;
+    address?: string;
+    // syntax for adding object key and properties
+    [property: string]: any;
+  }
+
+  let adminUser: IUser = {
+    name: "yogi",
+    age: 22,
+    lagu: "tikus makan sabun",
+    anotherKey: "example of another key"
+  };
+
+  console.log(adminUser.lagu);
+})();
