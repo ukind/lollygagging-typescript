8afb6ae111c57d6f52fc3530f7011419fb6b7fb2
Yogiswara Utama <utama.yogiswara@gmail.com>
2019-06-24 21:16:08 +0700

    interface access modifier

--- js/interface-class.js
+++ js/interface-class.js
@@ -1,24 +1,24 @@
 class Person {
-  constructor(name) {
-    this.name = name;
-  }
-  greet() {
-    return `Hello ${this.name}`;
-  }
+    constructor(name) {
+        this.name = name;
+    }
+    greet() {
+        return `Hello ${this.name}`;
+    }
 }
 let yogi = new Person("yogi");
 yogi.greet();
 class Heroism extends Person {
-  constructor(name) {
-    super(name);
-    this.superpowers = [];
-  }
-  addPower(power) {
-    this.superpowers.push(power);
-  }
-  listPower() {
-    return this.superpowers;
-  }
+    constructor(name) {
+        super(name);
+        this.superpowers = [];
+    }
+    addPower(power) {
+        this.superpowers.push(power);
+    }
+    listPower() {
+        return this.superpowers;
+    }
 }
 const batman = new Heroism("Utama");
 batman.addPower("Terbang");
