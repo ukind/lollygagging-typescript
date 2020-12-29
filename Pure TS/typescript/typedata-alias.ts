6ba973ee89af41a7d8da0f02a64ecd44d1894eb2
Yogiswara Utama <utama.yogiswara@gmail.com>
2020-12-29 19:20:07 +0700

    add alias typedata

---
+++ Pure TS/typescript/typedata-alias.ts
@@ -0,0 +1,25 @@
+type Second = number;
+const detik: Second = 10;
+
+type Triangle<type> = {
+  height: type;
+  length: type;
+};
+
+const segitiga: Triangle<number> = {
+  height: 10,
+  length: 14,
+};
+
+// Extending a type via intersections
+// type Animal = {
+//     name: string
+//   }
+  
+//   type Bear = Animal & { 
+//     honey: Boolean 
+//   }
+  
+//   const bear = getBear();
+//   bear.name;
+//   bear.honey;
